b4b410df226dc833f31c7b45bbbbb49e3f09382a
Joshuav773 <Joshuav773@gmail.com>
2021-03-24 12:18:04 -0400

    Add project files.

---
+++ PlayRight.Demo/AutomationBase/BaseDriver.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using PlaywrightSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayRight.Demo.AutomationBase
+{
+    public abstract class BaseDriver
+    {
+        protected static IBrowser Browser { get; private set; }
+        protected static IBrowserContext Context { get; private set; }
+        protected static IPage CurrentPage { get; private set; }
+
+        public BaseDriver()
+        {
+            if (Browser is null)
+            {
+                Task.Run(async () => await InitComponents()).Wait();
+            }
+        }
+
+        [SetUp]
+        protected async Task Navigate()
+        {
+            await Task.Run(async () =>
+            {
+                await CurrentPage.GoToAsync("https://www.google.com", waitUntil: LifecycleEvent.Load);
+            });
+        }
+
+        [TearDown]
+        protected async Task Flush()
+        {
+            await Task.Run(async () =>
+            {
+                await Browser.CloseAsync();
+            });
+        }
+
+        private async Task<IBrowser> GetBrowserAsync()
+        {
+            await Playwright.InstallAsync();
+            var playwright = await Playwright.CreateAsync();
+
+            return await playwright
+                    .Chromium
+                    .LaunchAsync(headless: false)
+                    ;
+        }
+
+        private async Task InitComponents()
+        {
+            var browser =  GetBrowserAsync();
+            var context =  browser.Result.NewContextAsync();
+            var page    =  context.Result.NewPageAsync();
+
+            await Task.WhenAll(browser, context, page);
+
+            Browser     = browser.Result;
+            Context     = context.Result;
+            CurrentPage = page.Result;
+        }
+    }
+}
---
+++ PlayRight.Demo/tests/SearchTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using PlayRight.Demo.AutomationBase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayRight.Demo.tests
+{
+    public class SearchTests : BaseDriver
+    {
+        [Test]
+        public async Task ICanClickTheSearchBar()
+        {
+            await CurrentPage.ClickAsync("//input[name='q']");
+        }
+    }
+}
